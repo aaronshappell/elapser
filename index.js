8b8ff74c34bc10f28e76f11f0d36b065574d5768
aaronshappell <crumster88@gmail.com>
2017-08-28 00:03:15 +0300

    Add screenshot function

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const {app, BrowserWindow, ipcMain} = require("electron");
+var fs = require("fs");
 const path = require("path");
 const url = require("url");
+var childProcess = require("child_process");
 
 let mainWindow;
 
 var createWindow = function(){
 	mainWindow = new BrowserWindow({width: 800, height: 600, show: false, frame: false});
 	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, "dist/index.html"),
+		pathname: path.join(__dirname, "dist", "index.html"),
 		protocol: "file:",
 		slashes: true
 	}));
@@ -17,6 +19,11 @@ var createWindow = function(){
 	});
 	mainWindow.on("ready-to-show", function(){
 		mainWindow.show();
+	});
+	screenshot(path.join(process.env.HOME, "Documents", "Elapser Timelapses", "test", "images", "test.jpg"), function(error){
+		if(error){
+			console.log(error);
+		}
 	});
 }
 
@@ -41,3 +48,27 @@ ipcMain.on("maximize", function(){
 ipcMain.on("close", function(){
 	mainWindow.close();
 });
+
+var screenshot = function(imagePath, callback){
+	var command;
+	switch(process.platform){
+		case "win32":
+			command = childProcess.spawn(path.join(__dirname, "bin", "win32", process.arch, "nircmdc.exe"), ["savescreenshot", imagePath]);
+			break;
+		case "darwin":
+			command = childProcess.spawn("screencapture", ["-x", imagePath]);
+			break;
+		case "linux":
+			command = childProcess.spawn("import", ["-window", "root", imagePath]);
+			break;
+		default:
+			callback("Error: os not supported");
+	}
+	command.on("close", function(code){
+		if(fs.existsSync(imagePath)){
+			callback(null);
+		} else{
+			callback("Error: screenshot not captured");
+		}
+	});
+}
